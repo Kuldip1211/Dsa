bd54cdad6cdde0f3725d9ad5207d0619d0f27bcf
Kuldip <kuldeepchudasama6999@gmail.com>
2024-06-19 23:31:04 +0530

    true

--- Array/Largest.java
+++ Array/Largest.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Largest {
  public static void main(String[] args) {
-  System.out.println("kanbha");
+  int arr[] = {1, 2, 3, 4, 5, 6};
+  System.out.println(Arrays.toString(arr));
+
+  int largest =  arr[0];
+  for (int i = 0; i < arr.length; i++) {
+   if(arr[i]>= arr[0]){
+    largest = arr[i];
+   }
+  }
+  System.out.println(largest);
  }
 }
